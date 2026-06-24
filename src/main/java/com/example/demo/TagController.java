dfb219848130949292f9d879017663208d6db67d
taetae-n <kthyn111@gmail.com>
2026-06-24 09:58:35 +0900

    TagController CORS 전체 허용

--- src/main/java/com/example/demo/TagController.java
+++ src/main/java/com/example/demo/TagController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class TagController {
 
     private final TagRepository tagRepository;
